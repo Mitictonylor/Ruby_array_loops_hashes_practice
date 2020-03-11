8f4708f4e5bc431aeb71dd33ef7ae0be06fb3f3d
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-11 18:06:28 +0000

    Add Antonio to the users hash

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -70,5 +70,9 @@ users["Erik"][:lottery_numbers].push(7)
 # 8. Change Erik's hometown to Edinburgh
 users["Erik"][:home_town] = "Edinburgh"
 # 9. Add a pet dog to Erik called "Fluffy"
-p users["Erik"][:pets].push(:name=>"Fluffy",:species =>"dog")
+users["Erik"][:pets].push(:name=>"Fluffy",:species =>"dog")
 # 10. Add another person to the users hash
+users["Antonio"] = {
+  :twitter => "fasske"
+}
+p users["Antonio"]
